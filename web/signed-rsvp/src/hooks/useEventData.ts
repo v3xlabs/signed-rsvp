873245bf2e39636f2222d189d7458710aa9b553c
Lucemans <luc@lucemans.nl>
2023-05-13 17:27:09 +0000

    Introduce useEventData Hook

---
+++ web/signed-rsvp/src/hooks/useEventData.ts
@@ -0,0 +1,23 @@
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
+
+const fetcher = url => fetch(url).then(r => r.json());
+
+type EventData = {
+    id: string;
+    text: string;
+    post_text: string;
+    image: string;
+    worldcoin_state?: string;
+    payload: string;
+};
+
+export const useEventData = () => {
+    const { query } = useRouter();
+    const { event_id } = query;
+    const v = useSWR<EventData>(event_id === null ? null : `https://api.signature.ceo/e/${event_id}`, fetcher);
+
+    console.log('eventdata');
+
+    return v;
+};
