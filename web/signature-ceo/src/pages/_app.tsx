189bc493ae2bc7e8be2b0cea46fa727924160475
Lucemans <luc@lucemans.nl>
2023-05-13 13:10:02 +0000

    Introduce Signature CEO

---
+++ web/signature-ceo/src/pages/_app.tsx
@@ -0,0 +1,48 @@
+import 'tailwindcss/tailwind.css';
+import '@/styles/styles.css';
+
+import { ConnectKitProvider, getDefaultClient } from 'connectkit';
+import { Inter } from 'next/font/google';
+import { useRouter } from 'next/router';
+import { createClient, WagmiConfig } from 'wagmi';
+import { polygon } from 'wagmi/chains';
+
+import { Layout } from '@/components/layout';
+
+const client = createClient(
+    getDefaultClient({
+        appName: 'Signature CEO',
+        chains: [polygon],
+    })
+);
+
+const inter = Inter({
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+    subsets: ['latin'],
+});
+
+const App = ({ Component, pageProps }) => {
+    const { pathname } = useRouter();
+
+    // eslint-disable-next-line no-undef
+    if (pathname === '/')
+        return (
+            <div className={inter.className}>
+                <Component {...pageProps} />
+            </div>
+        );
+
+    return (
+        <WagmiConfig client={client}>
+            <ConnectKitProvider theme="minimal" mode="light">
+                <div className={inter.className}>
+                    <Layout>
+                        <Component {...pageProps} />
+                    </Layout>
+                </div>
+            </ConnectKitProvider>
+        </WagmiConfig>
+    );
+};
+
+export default App;
---
+++ web/signature-ceo/src/pages/index.tsx
@@ -0,0 +1,12 @@
+
+function Home() {
+    return (
+        <div className="h-screen w-screen flex items-center justify-center">
+            <div className="flex flex-col justify-center items-center">
+                You lost the game.
+            </div>
+        </div>
+    );
+}
+
+export default Home;
