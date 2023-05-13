713cfeaf9d6593b2691248c423e41abf027a4817
Lucemans <luc@lucemans.nl>
2023-05-13 14:25:07 +0000

    Introduce mock event data

--- server/src/routes/e/get_event.ts
+++ server/src/routes/e/get_event.ts
@@ -1,5 +1,22 @@
 import { RouteHandler } from 'fastify';
 
-export const get_event: RouteHandler<{}> = async (request, reply) => {
-    return { hello: 'world' };
+type Event = {
+    text: string;
+    post_text: string;
+    image: string;
+    worldcoin_state?: string;
+    payload: string;
+};
+
+export const get_event: RouteHandler<{
+    Params: { event_id: string };
+}> = async (request, reply) => {
+    reply.send({
+        text: 'ronnie wants ur rsvp',
+        image: '/ronnie.png',
+        payload:
+            'I, {name}, solemly pinky promise I will come to rAave in Lisbon 23',
+        post_text: 'Ron E cnt w8 2 C U there!',
+        worldcoin_state: '12356789',
+    } as Event);
 };
