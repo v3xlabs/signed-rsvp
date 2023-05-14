a27befbe3665c0f635c6f25168f0d9c1524447b1
Lucemans <luc@lucemans.nl>
2023-05-14 07:49:18 +0000

    f

--- web/signature-ceo/src/components/sharebtn.tsx
+++ web/signature-ceo/src/components/sharebtn.tsx
@@ -4,10 +4,19 @@ export const ShareButton = () => {
         url: location.toString(),
     };
 
-    const canShare = navigator.canShare(shareData);
+    const canShare = navigator['canShare'] && navigator['canShare'](shareData);
 
     if (!canShare)
-        return <div>Cant share cuz ur browser don't support this feature.</div>;
+        return (
+            <button
+                className="btn"
+                onClick={() => {
+                    navigator.clipboard.writeText(shareData.url);
+                }}
+            >
+                Copy link
+            </button>
+        );
 
     return (
         <button
